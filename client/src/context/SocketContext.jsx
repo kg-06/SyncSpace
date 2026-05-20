1d466aa7fa8962daa7cca3f5d43f216c41579073
kg-06 <rk.keshav06@gmail.com>
2026-05-20 20:15:08 +0530

    made deploy ready

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -37,7 +37,7 @@ export const SocketProvider = ({ children }) => {
   }, [socketUserId]);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:5000");
+    const newSocket = io(import.meta.env.VITE_API_URL || "http://localhost:5000");
     setSocket(newSocket);
 
     return () => newSocket.disconnect();
